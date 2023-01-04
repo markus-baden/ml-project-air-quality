3a1cec2ac8291fdce761df320e361f9389301600
Markus Baden <markus-baden@gmx.de>
2023-01-04 09:35:07 +0100

    changed aq notebook + .py file; added xgboost to requirements

--- air_quality_functions.py
+++ air_quality_functions.py
@@ -6,7 +6,19 @@ import math
 import gc
 from pyts.preprocessing import InterpolationImputer
 
-
+def hazard_level(pm_2_5):
+    if pm_2_5 <= 12.0:
+        return 'good'
+    elif 12.0 < pm_2_5 <= 35.0:
+        return 'moderate'
+    elif 35.0 < pm_2_5 <= 55.0:
+        return 'unhealthy for sensitives'
+    elif 55.0 < pm_2_5 <= 150.0:
+        return 'unhealthy'
+    elif 150.0 < pm_2_5 <= 250.0:
+        return 'very unhealthy'  
+    elif 250.0 < pm_2_5:
+        return 'hazardous'
 
 def replace_nan(x):
     if x==" ":
@@ -20,12 +32,21 @@ def convert_str_to_list(data, features):
         data[feature]=data[feature].apply(lambda x: [ replace_nan(X) for X in x.replace("nan"," ").split(",")])
     return data
 
+def time_blocks(data, features, blocksize):
+    for feat in features:
+        for x in range(int(121/blocksize)):
+            sum = pd.Series(np.zeros(len(data)))
+            sum.index = data.index
+            for j in range(blocksize):
+                index = (x * blocksize + j)
+                if blocksize > 1:
+                    index += 1
+                sum += data[feat].str[index]
+            data[feat+ '_'+ str(x)] = sum / blocksize
+    return data.drop(features,axis=1)
 
 def impute_func(values, imputer, max_num_of_nans=80):
     num_of_nans = np.isnan(values).sum()
-    #if num_of_nans > max_num_of_nans:
-    #    return values, num_of_nans
-    #else:
     values = imputer.transform([values, np.arange(0, len(values))])
     return [list(values[0]), num_of_nans]
 
@@ -43,11 +64,11 @@ def impute_values(data, features, strategy='nearest'):
 
 
 def aggregate_features(x,col_name):
-    x["max_"+col_name]=x[col_name].apply(np.max)
-    x["min_"+col_name]=x[col_name].apply(np.min)
-    x["mean_"+col_name]=x[col_name].apply(np.mean)
-    x["std_"+col_name]=x[col_name].apply(np.std)
-    x["var_"+col_name]=x[col_name].apply(np.var)
-    x["median_"+col_name]=x[col_name].apply(np.median)
-    x["ptp_"+col_name]=x[col_name].apply(np.ptp)
+    x["max_"    + col_name] = x[col_name].apply(np.max)
+    x["min_"    + col_name] = x[col_name].apply(np.min)
+    x["mean_"   + col_name] = x[col_name].apply(np.mean)
+    x["std_"    + col_name] = x[col_name].apply(np.std)
+    x["var_"    + col_name] = x[col_name].apply(np.var)
+    x["median_" + col_name] = x[col_name].apply(np.median)
+    x["ptp_"    + col_name] = x[col_name].apply(np.ptp)
     return x  
